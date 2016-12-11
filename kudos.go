62463d7212a20d0177ce97033432ae15b935c4cf
mslangford <415langford@gmail.com>
2016-12-11 13:15:39 -0800

    remove main code

--- kudos.go
+++ kudos.go
@@ -30,10 +30,10 @@ type Transaction struct {
 }
 
 func main() {
-	err := shim.Start(new(SimpleChaincode))
-	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
-	}
+	//	err := shim.Start(new(SimpleChaincode))
+	//	if err != nil {
+	//		fmt.Printf("Error starting Simple chaincode: %s", err)
+	//	}
 }
 
 // Init resets all the things
